234a5ccce43a21df79885839be64255a4ab6344f
Pavle <dencic.pavle@gmail.com>
2025-09-30 20:44:15 +0200

    terraform remote state, actions workflow

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,6 +23,12 @@ terraform {
       version = "~> 1.3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "tfstate"
+    storage_account_name = "tfstate13416"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
