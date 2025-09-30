a35711908918cb4e2171332659ca3a09138e516b
Pavle <dencic.pavle@gmail.com>
2025-10-01 00:47:50 +0200

    move outputs to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "resource_group_name" {
 output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
+
+output "public_key_data" {
+  value = azapi_resource_action.ssh_public_key_gen.output.publicKey
+}
+output "private_key_data" {
+  value = azapi_resource_action.ssh_public_key_gen.output.privateKey
+  sensitive = true
+}
